dcddd3c48fc5b29719c5d50f36c2403dba3074fe
Ariel Tay <shihqian@gmail.com>
2017-11-08 16:46:16 +0800

    index.js file acting out

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// import React from "react";
+import React from "react"; // eslint-disable-line no-unused-vars
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
+import App from "./App"; // eslint-disable-line no-unused-vars
 
 ReactDOM.render(<App />, document.getElementById("root"));
