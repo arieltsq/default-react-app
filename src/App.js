5a64fdf06e76b9a3a6da917ee99ca947bce04ce0
Ariel Tay <shihqian@gmail.com>
2017-11-08 16:56:33 +0800

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react';
-import './assets/sass/main.css';
+import React from "react";
+import "./assets/sass/main.css";
 
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<h1 className="App-title"> Welcome to React </h1>{' '}
-				</header>{' '}
-				<p className="App-intro">
-					To get started, edit <code> src / App.js </code> and save to reload.{' '}
-				</p>{' '}
-				<p className="App-sass-test"> Test that SASS is working fine </p>{' '}
-			</div>
-		);
-	}
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title"> Welcome to React </h1>{" "}
+                </header>{" "}
+                <p className="App-intro">
+					To get started, edit <code> src / App.js </code> and save to reload.{" "}
+                </p>{" "}
+                <p className="App-sass-test"> Test that SASS is working fine </p>{" "}
+            </div>
+        );
+    }
 }
+// const App2 = () => (
+// <div className="App">
+// <header className="App-header">
+//     <h1 className="App-title"> Welcome to React </h1>{" "}
+// </header>{" "}
+// <p className="App-intro">
+//     To get started, edit <code> src / App.js </code> and save to reload.{" "}
+// </p>{" "}
+// <p className="App-sass-test"> Test that SASS is working fine </p>{" "}
+// </div>)
 
 export default App;
