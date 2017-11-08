fce076d9ba3052931de81929861fe42d0a07f991
Ariel Tay <shihqian@gmail.com>
2017-11-08 11:53:14 +0800

    setting up hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './assets/sass/main.css'
+import React, { Component } from "react";
+import "./assets/sass/main.css";
 
 class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className='App-sass-test'>
-          Test that SASS is working fine
-        </p>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+				<p className="App-sass-test">Test that SASS is working fine</p>
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
